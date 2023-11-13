02db626813543142f9e631367f34a01d42b5e1a8
baffeto <baffeto@mail.ru>
2023-11-13 22:56:15 +0300

    Test config

--- project/generator/urls.py
+++ project/generator/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import main_view
+
+app_name = 'generator'
+
 
 urlpatterns = [
-    path('', include('generator.urls', namespace='main')),
-    path('admin/', admin.site.urls),
+    path('', main_view)
 ]
